6728e71c01d405090f13c656b0a8b20f9071642d
vmuhembe <muhembelev@gmail.com>
2025-08-16 15:20:20 +0300

    Add SupabaseClient function to initialize Supabase client

---
+++ src/lib/supabas.js
@@ -0,0 +1,9 @@
+
+
+export default function SupabaseClient() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  return supabase;
+}
