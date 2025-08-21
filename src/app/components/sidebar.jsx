7c3077bf07df88000767262e941c9db0ec8150ae
vmuhembe <muhembelev@gmail.com>
2025-08-22 02:04:21 +0300

    fix: change title if we are in the activities page or home

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,76 +1,68 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import SearchBar from "./search";
 import {
   HomeIcon, BellIcon, ChatBubbleLeftIcon,
-  UserIcon, UserGroupIcon,PlusIcon
+  UserIcon, UserGroupIcon, PlusIcon
 } from '@heroicons/react/24/solid';
 
 export default function Sidebar() {
-  const router = useRouter();
-  const pathname = router.pathname;
 
-  const isGroupsPage = pathname === "/groups";
+  const pathname = (usePathname() || "").replace(/\/$/, "");
+  const isEventsPage = pathname === "/activities";
+
 
   return (
     <aside className="w-1/5 bg-blue-900 text-white min-h-full">
-      {isGroupsPage ? (
-           <div>
-      <h2 className="text-lg font-bold mb-4 text-center">Groups</h2>
-      <ul className="space-y-2">
-        <li>
+
+
+      <ul className="w-full">
+        <li className="p-4 border-b border-gray-100/20">
+          {
+            isEventsPage ? (
+              <h2 className="text-lg font-bold mb-4 text-center">Activities</h2>
+
+            ) : (
+              <Link href="/" className="flex w-full  pb-4 items-center">
+                <HomeIcon className="h-6 w-6 mr-2" /> Home
+              </Link>
+            )
+          }
           <SearchBar />
         </li>
-        <li>
-          <Link
-            href="/create-group"
-            className="block px-4 py-2 flex  text-white hover:bg-blue-700 hover:shadow-md transition duration-200"
-          >
-            <PlusIcon className="h-6 w-6 mr-1"/>
-            Create Group
+        <li className="p-4 border-b border-gray-100/20">
+          <Link href="/profile" className="flex w-full  pb-4 items-center">
+            <UserIcon className="h-6 w-6 mr-2" /> Profile
           </Link>
         </li>
-        <li>
+        <li className="p-4 border-b border-gray-100/20">
+          <Link href="/messages" className="flex w-full  pb-4 items-center">
+            <ChatBubbleLeftIcon className="h-6 w-6 mr-2" /> Messages
+          </Link>
+        </li>
+        <li className="p-4 border-b border-gray-100/20">
+          <Link href="/activities" className="flex w-full  pb-4 items-center">
+            <UserGroupIcon className="h-6 w-6 mr-2" /> Activities
+          </Link>
+        </li>
+        <li className="p-4 border-b border-gray-100/20">
           <Link
-            href="/groups"
-            className="block px-4 py-2 r  text-white hover:bg-blue-700 hover:shadow-md transition duration-200"
+            href="/create_activity"
+            className="flex w-full  pb-4 items-center"
           >
-            Your Groups
+            <PlusIcon className="h-6 w-6 mr-2" />
+            Create Activity
+          </Link>
+        </li>
+        <li className="p-4 border-b border-gray-100/20">
+          <Link href="/notifications" className="flex w-full  pb-4 items-center">
+            <BellIcon className="h-6 w-6 mr-2" /> Notifications
           </Link>
         </li>
       </ul>
-    </div>
-      ) : (
-        <ul className="w-full">
-          <li className="p-4 border-b border-gray-100/20">
-            <Link href="/" className="flex w-full  pb-4 items-center">
-              <HomeIcon className="h-6 w-6 mr-2" /> Home
-            </Link> <SearchBar/>
-          </li>
-          <li className="p-4 border-b border-gray-100/20">
-            <Link href="/profile" className="flex w-full  pb-4 items-center">
-              <UserIcon className="h-6 w-6 mr-2" /> Profile
-            </Link>
-          </li>
-          <li className="p-4 border-b border-gray-100/20">
-            <Link href="/messages" className="flex w-full  pb-4 items-center">
-              <ChatBubbleLeftIcon className="h-6 w-6 mr-2" /> Messages
-            </Link>
-          </li>
-          <li className="p-4 border-b border-gray-100/20">
-            <Link href="/groups" className="flex w-full  pb-4 items-center">
-              <UserGroupIcon className="h-6 w-6 mr-2" /> Groups
-            </Link>
-          </li>
-          <li className="p-4 border-b border-gray-100/20">
-            <Link href="/notifications" className="flex w-full  pb-4 items-center">
-              <BellIcon className="h-6 w-6 mr-2" /> Notifications
-            </Link>
-          </li>
-        </ul>
-      )}
+
     </aside>
   );
 }
