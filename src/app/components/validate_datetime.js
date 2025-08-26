b2a51f30600ae9901c6195067eb711cfa45dd614
vmuhembe <muhembelev@gmail.com>
2025-08-26 11:59:09 +0300

    feat: add two functions to validate date and time for an event.

---
+++ src/app/components/validate_datetime.js
@@ -0,0 +1,21 @@
+export default function validateEventDate(date) {
+    const current_year = new Date(Date.now())
+    const event_year = new Date(date)
+    if (event_year < current_year) {
+        return new Error("The event date cannot be before now")
+    }
+
+}
+
+export default function validateEventTime(startTime, endTime) {
+    const [sh, sm] = startTime.split(":").map(Number)
+    const [eh, em] = endTime.split(":").map(Number)
+
+    const startTimeInMinutes = (sh * 60) + sm
+    const EndTimeInMinutes = (eh * 60) + em
+
+    if (startTimeInMinutes + 30 > EndTimeInMinutes) {
+        return new Error("An event takes atleast 30 minutes")
+    }
+
+}
