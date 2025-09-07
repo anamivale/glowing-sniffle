fcc31d40923c651c351218f79c33f4925db3e70b
anamivale <muhembelev@gmail.com>
2025-09-07 22:51:36 +0300

    feat: import  user from getuser component

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,17 +1,29 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import SearchBar from "./search";
 import {
   HomeIcon, BellIcon, ChatBubbleLeftIcon,
   UserIcon, UserGroupIcon, PlusIcon
 } from '@heroicons/react/24/solid';
+import { getBrowserSupabase } from "@/lib/supabas";
+import { useState } from "react";
+import GetUser from "./getUser";
 
 export default function Sidebar() {
 
   const pathname = (usePathname() || "").replace(/\/$/, "");
   const isEventsPage = pathname === "/events";
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const supabase = getBrowserSupabase();
+  const router = useRouter();
+
+  // Fetch user session and handle authentication state changes
+  GetUser(setError, setLoading, setUser, supabase, router);
 
 
   return (
@@ -32,11 +44,14 @@ export default function Sidebar() {
           }
           <SearchBar />
         </li>
-        <li className="p-4 border-b border-gray-100/20">
-          <Link href="/profile" className="flex w-full  pb-4 items-center">
-            <UserIcon className="h-6 w-6 mr-2" /> Profile
-          </Link>
-        </li>
+        {user && (
+          <li className="p-4 border-b border-gray-100/20">
+            <Link href={`/profile/${user.id}`} className="flex w-full  pb-4 items-center">
+              <UserIcon className="h-6 w-6 mr-2" /> Profile
+            </Link>
+          </li>
+        )}
+
         <li className="p-4 border-b border-gray-100/20">
           <Link href="/messages" className="flex w-full  pb-4 items-center">
             <ChatBubbleLeftIcon className="h-6 w-6 mr-2" /> Messages
