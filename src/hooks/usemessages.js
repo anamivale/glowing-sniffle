f922aab1ce45932982c22d3f0cd8589ad352fe36
anamivale <muhembelev@gmail.com>
2025-11-29 17:56:57 +0300

    feat : insert message into db

--- src/hooks/usemessages.js
+++ src/hooks/usemessages.js
@@ -2,13 +2,27 @@ import { getBrowserSupabase } from "@/lib/supabas"
 
 export async function getConvMessages(conversation_id) {
     const supabase = getBrowserSupabase()
-    const {data: messages} = await supabase
-    .from("messages")
-    .select("*")
-    .eq('conversation_id',conversation_id )
-    .order("created_at", {ascending:true})
+    const { data: messages } = await supabase
+        .from("messages")
+        .select("*")
+        .eq('conversation_id', conversation_id)
+        .order("created_at", { ascending: true })
 
     return messages
 
 
+}
+
+
+export async function insertMessage() {
+    const supabase = getBrowserSupabase()
+    const {  error } = await supabase
+        .from('messages')
+        .insert({
+            conversation_id: conversation_id,
+            sender_id: currentUserId,
+            content: content
+
+        })
+    return {  error }
 }
